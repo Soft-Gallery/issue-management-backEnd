95f3d0f87bd3cbb7569061df21aadb6cf65fb36f
Seonho Kim <5061ksh@naver.com>
2024-05-14 01:23:00 +0900

    Initial commit

---
+++ src/main/java/com/softgallery/issuemanagementbackEnd/IssueManagementBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.softgallery.issuemanagementbackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueManagementBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueManagementBackEndApplication.class, args);
+	}
+
+}
