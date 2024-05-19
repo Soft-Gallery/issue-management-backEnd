9b425fa0aae16ac3843cf68d39ce8a7e6cf98ab0
RaccHoon <choco730522@gmail.com>
2024-05-19 20:00:16 +0900

    feat:fetch user entity. generate custom annotation

--- src/main/java/com/softgallery/issuemanagementbackEnd/entity/UserEntity.java
+++ src/main/java/com/softgallery/issuemanagementbackEnd/entity/UserEntity.java
@@ -1,7 +1,10 @@
 package com.softgallery.issuemanagementbackEnd.entity;
 
+import com.softgallery.issuemanagementbackEnd.util.custom_annotation.IDRule;
+import com.softgallery.issuemanagementbackEnd.util.custom_annotation.PasswordRule;
 import com.softgallery.issuemanagementbackEnd.util.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
@@ -12,21 +15,22 @@ import lombok.Setter;
 @Table(name = "user")
 public abstract class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    @NonNull
+    @IDRule
+    @Column(name = "user_id", unique = true)
+    private String userId;
 
     @NonNull
     private String name;
 
     @NonNull
+    @Email(message = "이메일 주소 형식이 올바르지 않습니다")
     private String email;
 
     @NonNull
+    @PasswordRule
     private String password;
 
-    // @Enumerated(EnumType.STRING): DB에는 Enum 타입의 이름(ex admin)이 들어감.
-    // 프로그래밍시에는 열거형으로 처리됨
-    // 대신 role 타입 이외의 값이 들어오는것 못 막음
     @NonNull
     @Enumerated(EnumType.STRING)
     private Role role;
