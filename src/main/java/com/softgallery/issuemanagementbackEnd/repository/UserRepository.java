b4ed9958a58f93cab05c999cdaad079c4483e438
RaccHoon <choco730522@gmail.com>
2024-05-19 23:45:57 +0900

    fix:dependency problem. fixing...

--- src/main/java/com/softgallery/issuemanagementbackEnd/repository/UserRepository.java
+++ src/main/java/com/softgallery/issuemanagementbackEnd/repository/UserRepository.java
@@ -5,6 +5,6 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByUserId(Long id);
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+    UserEntity findByUserId(String id);
 }
---
+++ src/main/java/com/softgallery/issuemanagementbackEnd/service/user/Role.java
@@ -0,0 +1,8 @@
+package com.softgallery.issuemanagementbackEnd.service.user;
+
+public enum Role {
+    ADMIN,
+    TEST,
+    PL,
+    DEVELOPER
+}
