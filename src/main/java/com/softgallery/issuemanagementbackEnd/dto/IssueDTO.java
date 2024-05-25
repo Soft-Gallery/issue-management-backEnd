b67cc94ff7f4c0a8fd33f69361fc65143c668449
Seonho Kim <5061ksh@naver.com>
2024-05-26 00:19:46 +0900

    refactor : adapt changes of issue entity (field, constructor)

--- src/main/java/com/softgallery/issuemanagementbackEnd/dto/IssueDTO.java
+++ src/main/java/com/softgallery/issuemanagementbackEnd/dto/IssueDTO.java
@@ -2,6 +2,7 @@ package com.softgallery.issuemanagementbackEnd.dto;
 
 import com.softgallery.issuemanagementbackEnd.entity.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.softgallery.issuemanagementbackEnd.service.issue.Priority;
@@ -20,22 +21,25 @@ public class IssueDTO {
     private UserDTO fixer;
     private Long projectId;
     private List<CommentEntity> comments;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate = LocalDateTime.MIN;
 
     public IssueDTO() { }
 
     public IssueDTO(final Long id, final String title, final String description, final UserDTO reporter,
-                    final State status, final Priority priority) {
+                    final State status, final Priority priority, final LocalDateTime startDate) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.reporter = reporter;
         this.status = status;
         this.priority = priority;
+        this.startDate = startDate;
     }
 
     public IssueDTO(final Long id, final String title, final String description, final UserDTO reporter,
                     final State status, final Priority priority,
-                    final UserDTO assignee) {
+                    final UserDTO assignee, final LocalDateTime startDate) {
         this.id = id;
         this.title = title;
         this.description = description;
@@ -43,11 +47,14 @@ public class IssueDTO {
         this.status = status;
         this.priority = priority;
         this.assignee = assignee;
+        this.startDate = startDate;
     }
 
     public IssueDTO(final Long id, final String title, final String description, final UserDTO reporter,
                     final State status, final Priority priority,
-                    final UserDTO assignee, final List<CommentEntity> comments, final UserDTO fixer, final Long projectId) {
+                    final UserDTO assignee, final List<CommentEntity> comments,
+                    final UserDTO fixer, final Long projectId,
+                    final LocalDateTime startDate) {
         this.id = id;
         this.title = title;
         this.description = description;
@@ -58,5 +65,25 @@ public class IssueDTO {
         this.comments = comments;
         this.fixer = fixer;
         this.projectId = projectId;
+        this.startDate = startDate;
+    }
+
+    public IssueDTO(final Long id, final String title, final String description, final UserDTO reporter,
+                    final State status, final Priority priority,
+                    final UserDTO assignee, final List<CommentEntity> comments,
+                    final UserDTO fixer, final Long projectId,
+                    final LocalDateTime startDate, final LocalDateTime endDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.reporter = reporter;
+        this.status = status;
+        this.priority = priority;
+        this.assignee = assignee;
+        this.comments = comments;
+        this.fixer = fixer;
+        this.projectId = projectId;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 }
