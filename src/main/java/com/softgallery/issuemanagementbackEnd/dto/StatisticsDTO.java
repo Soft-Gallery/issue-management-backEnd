7fed2d39af9203fa19ba116a729751e7ee6433dc
Seonho Kim <5061ksh@naver.com>
2024-05-27 00:06:19 +0900

    refactor : delete state and priority field(column)

--- src/main/java/com/softgallery/issuemanagementbackEnd/dto/StatisticsDTO.java
+++ src/main/java/com/softgallery/issuemanagementbackEnd/dto/StatisticsDTO.java
@@ -1,8 +1,6 @@
 package com.softgallery.issuemanagementbackEnd.dto;
 
 import com.softgallery.issuemanagementbackEnd.service.issue.MainCause;
-import com.softgallery.issuemanagementbackEnd.service.issue.Priority;
-import com.softgallery.issuemanagementbackEnd.service.issue.State;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -13,13 +11,11 @@ public class StatisticsDTO {
     private Long id;
     private Long issueId;
     private Long projectId;
-    private Priority priority;
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime startDate;
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime endDate = LocalDateTime.of(1990, 1, 1, 0, 0,0 );
     private Long duration;
-    private State state;
     private MainCause mainCause;
 
     // Duration(시간차) Long 값으로 전달
@@ -31,41 +27,35 @@ public class StatisticsDTO {
 
     public StatisticsDTO() { }
 
-    public StatisticsDTO(final Long issueId, final Long projectId, final Priority priority,
-                         final LocalDateTime startDate, final LocalDateTime endDate, final State state) {
+    public StatisticsDTO(final Long issueId, final Long projectId,
+                         final LocalDateTime startDate, final LocalDateTime endDate) {
         this.issueId = issueId;
         this.projectId = projectId;
-        this.priority = priority;
         this.startDate = startDate;
         this.endDate = endDate;
         this.duration = calculateDuration();
-        this.state = state;
     }
 
-    public StatisticsDTO(final Long issueId, final Long projectId, final Priority priority, final LocalDateTime startDate,
-                         final LocalDateTime endDate, final State state,
+    public StatisticsDTO(final Long issueId, final Long projectId, final LocalDateTime startDate,
+                         final LocalDateTime endDate,
                          final MainCause mainCause) {
         this.issueId = issueId;
         this.projectId = projectId;
-        this.priority = priority;
         this.startDate = startDate;
         this.endDate = endDate;
         this.duration = calculateDuration();
-        this.state = state;
         this.mainCause = mainCause;
     }
 
-    public StatisticsDTO(final Long id, final Long issueId, final Long projectId, final Priority priority, final LocalDateTime startDate,
-                         final LocalDateTime endDate, final State state,
+    public StatisticsDTO(final Long id, final Long issueId, final Long projectId, final LocalDateTime startDate,
+                         final LocalDateTime endDate,
                          final MainCause mainCause) {
         this.id = id;
         this.issueId = issueId;
         this.projectId = projectId;
-        this.priority = priority;
         this.startDate = startDate;
         this.endDate = endDate;
         this.duration = calculateDuration();
-        this.state = state;
         this.mainCause = mainCause;
     }
 }
