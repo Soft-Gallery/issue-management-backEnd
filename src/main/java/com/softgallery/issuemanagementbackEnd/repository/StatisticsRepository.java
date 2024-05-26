9fd0ab9dd6eafc1c32b175dc6f2237757677a2ce
Seonho Kim <5061ksh@naver.com>
2024-05-26 17:13:41 +0900

    refactor : add methods for querying the number of issues between certain duration range

--- src/main/java/com/softgallery/issuemanagementbackEnd/repository/StatisticsRepository.java
+++ src/main/java/com/softgallery/issuemanagementbackEnd/repository/StatisticsRepository.java
@@ -19,18 +19,18 @@ public interface StatisticsRepository extends JpaRepository<StatisticsEntity, Lo
 
     // Method for statistics derived by all issues
     Long countByPriority(Priority priority);
-    Long countByDuration(Long duration);
     Long countByMainCause(MainCause mainCause);
-//    Long countByState(State state);
+    Long countByState(State state);
+    Long countByDurationBetween(Long lowerDuration, Long higherDuration);
 
 
     // Method for statistics derived by issues in a certain project
     // @Query("SELECT COUNT(*) FROM project_member WHERE projectId={projectId} AND priority={priority}")
     Long countByProjectIdAndPriority(Long projectId, Priority priority);
-    Long countByProjectIdAndDuration(Long projectId, Long duration);
     Long countByProjectIdAndMainCause(Long projectId, MainCause mainCause);
     Long countByProjectId(Long projectId);
-//    Long countByProjectIdAndState(Long projectId, State state);
+    Long countByProjectIdAndState(Long projectId, State state);
+    Long countByProjectIdAndDurationBetween(Long projectId, Long lowerDuration, Long upperDuration);
 
     @Override
     void deleteById(Long aLong);
