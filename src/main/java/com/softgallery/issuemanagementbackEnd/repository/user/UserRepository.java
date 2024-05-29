e7941647089d19f74b9805e68546b9d72442eabe
RaccHoon <choco730522@gmail.com>
2024-05-29 17:04:39 +0900

    feat: make new field role. this column doesnt update in database

--- src/main/java/com/softgallery/issuemanagementbackEnd/repository/user/UserRepository.java
+++ src/main/java/com/softgallery/issuemanagementbackEnd/repository/user/UserRepository.java
@@ -1,12 +1,16 @@
 package com.softgallery.issuemanagementbackEnd.repository.user;
 
 import com.softgallery.issuemanagementbackEnd.entity.user.UserEntity;
+import com.softgallery.issuemanagementbackEnd.service.user.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     UserEntity findByUserId(String id);
 
+    List<UserEntity> findAllByRole(Role role);
     Boolean existsByUserId(String id);
 }
